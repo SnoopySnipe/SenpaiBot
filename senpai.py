f34f4e30240528c56a21a6d1afbef8e711a483fd
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-08-23 13:15:22 -0400

    add support for passing in token as a command line arguments

--- senpai.py
+++ senpai.py
@@ -1,8 +1,19 @@
+import sys
 import discord
 from discord.ext import commands
 import random
 
 description = '''The senpai of the server.'''
+
+# amount of arguments
+argc = len(sys.argv)
+# bot token
+token = ""
+
+# parse command line arguments
+for i in range(argc):
+    if (sys.argv[i] == "-t" and i < argc):
+        token = sys.argv[i+1]
 
 bot = commands.Bot(command_prefix='!', description=description)
 
@@ -12,10 +23,10 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    
+
 @bot.command()
 async def ball(question : str):
     """Answers your question with a yes or no."""
     await bot.say('Yes.')
 
-bot.run("token")
+bot.run(token)
