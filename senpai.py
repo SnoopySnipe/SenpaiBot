2eadf47706be655f54dcc6468441d8f65d9d03b6
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-08-23 17:53:03 -0400

    fix questions being cut off

--- senpai.py
+++ senpai.py
@@ -17,6 +17,28 @@ for i in range(argc):
 
 bot = commands.Bot(command_prefix='!', description=description)
 
+answers = ["It is certain",
+           "It is decidedly so",
+           "Without a doubt",
+           "Yes definitely",
+           "You may rely on it",
+           "As I see it yes",
+           "Most likely",
+           "Outlook good",
+           "Yes",
+           "Signs point to yes",
+           "Reply hazy try again",
+           "Ask again later",
+           "Better not tell you now",
+           "Cannot predict now",
+           "Concentrate and ask again",
+           "Don't count on it",
+           "My reply is no",
+           "My sources say no",
+           "Outlook not so good",
+           "Very doubtful"]
+num_answers = len(answers) - 1
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -24,29 +46,24 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def fortune(question : str):
+@bot.event
+async def on_message(message):
     """Answers your question with a yes or no."""
-    answers = ["It is certain", \
-               "It is decidedly so", \
-               "Without a doubt", \
-               "Yes definitely", \
-               "You may rely on it", \
-               "As I see it yes", \
-               "Most likely", \
-               "Outlook good", \
-               "Yes", \
-               "Signs point to yes", \
-               "Reply hazy try again", \
-               "Ask again later", \
-               "Better not tell you now", \
-               "Cannot predict now", \
-               "Concentrate and ask again", \
-               "Don't count on it", \
-               "My reply is no", \
-               "My sources say no", \
-               "Outlook not so good", \
-               "Very doubtful"]
-    await bot.say("`" + answers[random.randint(0, len(answers))-1] + "`")
+    message_content = message.content
+
+    if (message_content.startswith("!fortune")):
+        offset = len("!fortune")
+        question = message_content[offset+1:]
+        reply = ("`Question: " + question + "\n" +
+          "Answer: " + answers[random.randint(0, num_answers)] + "`")
+
+        await bot.send_message(message.channel, reply)
+
+# @bot.command()
+# async def fortune(question : str):
+
+#    reply = "`" + answers[random.randint(0, num_answers)] + "`"
+#    await bot.say(reply)
 
 bot.run(token)
+bot.logout()
