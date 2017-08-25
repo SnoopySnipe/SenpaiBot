c3ef1c9f41605ab65bb036b60e6308d5ea39871f
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-08-25 13:06:47 -0400

    move all functions related to voice and music playing into player.py - add support for a global volume that persists between songs

---
+++ player.py
@@ -0,0 +1,94 @@
+import asyncio
+
+import discord
+
+# song queue
+queue = []
+# players
+player_list = []
+
+player_volume = 45
+
+async def join_voice_channel_of_user(message, bot):
+    '''(Message) -> VoiceClient
+    given the message of a user, join their voice channel if they are in one
+    '''
+
+    bot_voice = False
+    # get the sender of the messge
+    author = message.author
+    # get their current voice channel
+    cur_author_vchannel = author.voice.voice_channel
+    # if they are in a voice channel, join it
+    if (cur_author_vchannel):
+        bot_voice = await bot.join_voice_channel(cur_author_vchannel)
+    return bot_voice
+
+
+async def leave_all_voice_channels(bot):
+    '''(Client) -> null
+    makes the Client leave all connected voice channels
+    '''
+
+    connected_voices = []
+    # get a list of all voice channels the bot is connected to
+    for voice in bot.voice_clients:
+        # if bot is connected to the channel, add it to the list
+        if (voice.is_connected()):
+            connected_voices.append(voice)
+    # if bot is not connected to any voice channel, print error message
+    if (connected_voices):       
+        # disconnect bot from all connected voice channels
+        for voice in connected_voices:
+            await voice.disconnect()
+
+async def play_song(bot, bot_voice, message):
+    '''(VoiceClient, Message) -> null
+    Plays songs in the queue.
+    '''    
+    # play songs while there are songs in the queue
+    while (queue):
+        # pop the next song off the queue
+        url = queue.pop(0)
+        # retrieve song
+        player = await bot_voice.create_ytdl_player(url)
+        # play the song
+        player.start()
+        player_list.append(player)
+        await player_set_volume(player_volume)
+        # print a message showing what is currently playing
+        reply = ("`Playing: \"" + player.title + "\"`")
+        await bot.send_message(message.channel, reply)
+        # if the bot is no longer playing anything, stop the player and leave
+        while (not player.is_done()):
+            await asyncio.sleep(3)
+        player.stop()
+        player_list.pop()
+    # output message saying bot has left and leave
+    reply = "`Leaving voice channel.`"
+    await bot.send_message(message.channel, reply)     
+    await leave_all_voice_channels(bot)
+
+
+async def player_skip():
+    '''(null) -> null
+    Skips the next song in queue.
+    '''
+    for player in player_list:
+        player.stop()
+
+async def player_pause():
+    for player in player_list:
+        player.pause()
+
+async def player_resume():
+    for player in player_list:
+        player.resume()
+
+def player_get_volume():
+    return player_volume
+
+async def player_set_volume(volume):
+    for player in player_list:
+        player.volume = volume / 50
+
--- senpai.py
+++ senpai.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 import random
 
+import player
 from bot_answers import *
 
 description = '''The senpai of the server.'''
@@ -21,10 +22,6 @@ for i in range(argc):
 
 # initialize bot
 bot = commands.Bot(command_prefix='!', description=description)
-# song queue
-queue = []
-# players
-player_list = []
 
 # keep track of volume
 #global_volume = 1
@@ -35,8 +32,7 @@ def signal_handler(signal, frame):
     '''
     
     print("\nLogging out bot...")
-    queue.clear()
-    player_skip()
+    player.queue.clear()
     # log out bot and close connection
     bot.logout()
     bot.close()
@@ -46,76 +42,6 @@ def signal_handler(signal, frame):
 
 # map Ctrl+C to trigger signal_handler function
 signal.signal(signal.SIGINT, signal_handler)
-
-
-async def join_voice_channel_of_user(message):
-    '''(Message) -> VoiceClient
-    given the message of a user, join their voice channel if they are in one
-    '''
-
-    bot_voice = False
-    # get the sender of the messge
-    author = message.author
-    # get their current voice channel
-    cur_author_vchannel = author.voice.voice_channel
-    # if they are in a voice channel, join it
-    if (cur_author_vchannel):
-        bot_voice = await bot.join_voice_channel(cur_author_vchannel)
-    return bot_voice
-
-
-async def leave_all_voice_channels(bot):
-    '''(Client) -> null
-    makes the Client leave all connected voice channels
-    '''
-
-    connected_voices = []
-    # get a list of all voice channels the bot is connected to
-    for voice in bot.voice_clients:
-        # if bot is connected to the channel, add it to the list
-        if (voice.is_connected()):
-            connected_voices.append(voice)
-    # if bot is not connected to any voice channel, print error message
-    if (connected_voices):       
-        # disconnect bot from all connected voice channels
-        for voice in connected_voices:
-            await voice.disconnect()
-
-
-async def play_video(bot_voice, message):
-    '''(VoiceClient, Message) -> null
-    Plays songs in the queue.
-    '''    
-    # play songs while there are songs in the queue
-    while (queue):
-        # pop the next song off the queue
-        url = queue.pop(0)
-        # play the song
-        player = await bot_voice.create_ytdl_player(url)
-        # adjust the volume
-        #player.volume = global_volume
-        player.start()
-        player_list.append(player)
-        # print a message showing what is currently playing
-        reply = ("`Playing: \"" + player.title + "\"`")
-        await bot.send_message(message.channel, reply)
-        # if the bot is no longer playing anything, stop the player and leave
-        while (not player.is_done()):
-            await asyncio.sleep(3)
-        player.stop()
-        player_list.pop()
-    # output message saying bot has left and leave
-    reply = "`Leaving voice channel.`"
-    await bot.send_message(message.channel, reply)     
-    await leave_all_voice_channels(bot)
-
-
-async def player_skip():
-    '''(null) -> null
-    Skips the next song in queue.
-    '''
-    for player in player_list:
-        player.stop()
 
 
 @bot.event
@@ -193,18 +119,18 @@ async def on_message(message):
 
         # if bot is not already connected,
         # then connect them to voice channel
-        if (not bot_voice):
-            bot_voice = await join_voice_channel_of_user(message)
+        if (not already_connected):
+            bot_voice = await player.join_voice_channel_of_user(message, bot)
         # if bot is connected
         if (bot_voice):
             # check if it is a valid url
             if (url.startswith("http")):
                 # add the url to the queue
-                queue.append(url)
+                player.queue.append(url)
                 # if the bot wasn't already connected, join voice channel
                 # and play the video
                 if (not already_connected):
-                    await play_video(bot_voice, message)
+                    await player.play_song(bot, bot_voice, message)
                 # if the bot was already connected, just print a message saying
                 # that the song was queued
                 else:
@@ -213,56 +139,51 @@ async def on_message(message):
             
             # skip to the next song in queue
             elif (url == "skip"):
-                await player_skip()
+                await player.player_skip()
                 
             # clear the queue and skip
             elif (url == "stop"):
-                queue.clear()
-                await player_skip()
+                player.queue.clear()
+                await player.player_skip()
                 
             # display the song queue
             elif (url == "queue"):
                 reply = "`"
-                if len(queue) == 0:
+                if (len(player.queue) == 0):
                     reply = "`Queue is empty.`"
                 else:
-                    for i in range(len(queue)):
-                        if i != 0:
-                            reply += "\n"
-                        reply += str(i) + "\t" * 4 + queue[i]
+                    for i in range(len(player.queue)):
+                        reply += str(i) + "." + "\t" * 4 + player.queue[i] + "\n"
                     reply += "`"
                 await bot.send_message(message.channel, reply)
                 
             # pause the music
             elif (url == "pause"):
-                for player in player_list:
-                    player.pause()
+                await player.player_pause()
                     
             # resume the music
             elif (url == "resume"):
-                for player in player_list:
-                    player.resume()
+                await player.player_resume()
             
             # adjust the music volume
             elif (url.startswith("volume")):
                 volume_offset = len("volume")
-                volume = url[volume_offset+1 : ]
-                # display current volume
-                if volume == "":
-                    for player in player_list:
-                        vol = player.volume
-                    reply = "`Volume is currently at " + str(vol * 50) + ".`"
-                # if valid volume, adjust it right now and globally
-                elif (float(volume) >= 0 and float(volume) <= 100):
-                    for player in player_list:
-                        player.volume = float(volume) / 50
-                    # adjust the global volume
-                    #global_volume = float(volume) / 50
-                    reply = "`Volume has been adjusted to " + str(volume) + ".`"
-                # prompt for a valid volume if invalid
+                volume = url[volume_offset+1:]
+                if (len(volume) <= 0):
+                    volume = player.player_get_volume()
+                    reply = "`Volume is currently at " + str(volume) + ".`"
                 else:
-                    reply = "`Please enter a volume between 0 and 100.`"
-                await bot.send_message(message.channel, reply)                
+                    try:
+                        # if valid volume, adjust it
+                        volume = float(volume)
+                        player.player_volume = volume
+                        await player.player_set_volume(player.player_volume)
+                        reply = ("`Volume has been adjusted to " +
+                            str(player.player_volume) + ".`")
+                    # prompt for a valid volume if invalid
+                    except ValueError:
+                        reply = "`Please enter a volume between 0 and 100.`"
+                await bot.send_message(message.channel, reply)
             
             # search for a song
             #else:
