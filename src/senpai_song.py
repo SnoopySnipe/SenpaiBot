f7b83e0ff624f7688df187b283d38a32974fbfe1
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-01-03 12:56:12 -0500

    allow songs to not have title for now

--- src/senpai_song.py
+++ src/senpai_song.py
@@ -2,8 +2,12 @@ import os
 
 class SenpaiSong:
 
-    def __init__(self, file_path, title):
-        self.file_path = file_path
+    def __init__(self, path, title="No Song Title"):
+        '''(SenpaiSong, str, str) -> SenpaiSong
+        path: where to find the song. can be https:// or normal path.
+        title: title of song.
+        '''
+        self.path = path
         self.title = title
         self.ref = 1
 
@@ -11,3 +15,6 @@ class SenpaiSong:
         self.ref -= 1
         if (self.ref == 0):
             os.remove(self.file_path)
+
+    def __str__(self):
+        return self.title
