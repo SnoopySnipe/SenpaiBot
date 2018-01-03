29acc3270b2c2afbe79516bc7634a32199fc443b
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-01-03 12:55:21 -0500

    change constants to all caps

--- src/download_youtube.py
+++ src/download_youtube.py
@@ -1,23 +1,23 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-download_dir = "/tmp/"
-audio_format = "wav"
+DOWNLOAD_DIR = "/tmp/"
+AUDIO_FORMAT = "wav"
 
 ydl_opts = {
     'format': 'bestaudio/best',
-    'outtmpl': download_dir + "%(title)s.mp4",
+    'outtmpl': DOWNLOAD_DIR + "%(title)s.mp4",
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
 # for high quality audio in exchange for audio stutters
 #       'preferredcodec': 'flac',
-        'preferredcodec': audio_format,
+        'preferredcodec': AUDIO_FORMAT,
         'preferredquality': '192',
     }],
 }
 
 def download_song(url):
-
+    ''' '''
     ydl = youtube_dl.YoutubeDL(ydl_opts)
     info_dict = ydl.extract_info(url)
     return info_dict
