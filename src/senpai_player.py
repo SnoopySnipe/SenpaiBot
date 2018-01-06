47a51c967509156812f21bd7222748377c4164bb
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-01-05 21:06:13 -0500

    remove os things from senpai_song - SenpaiPlayer has been separated to SenpaiPlayer and SenpaiPlayerLocal for local songs - add reference system to SenpaiPlayer and SenpaiPlayerLocal to prevent excess network usage - rename SenpaiPlayer methods

--- src/senpai_player.py
+++ src/senpai_player.py
@@ -1,9 +1,141 @@
 import asyncio
+import os
 
 import discord
 
+import download_youtube
+
+from helpers import *
+
 class UserNotInVoiceChannelException(Exception):
     '''This exception is thrown when a user is not in a voice channel'''
+
+class SenpaiPlayerLocal:
+
+    def __init__(self, delay=3):
+        '''
+        _client : Client
+        _queue : list
+        _refcount_dict : dict
+        _player_list :
+        _player_volume :
+        _roundtrip_delay : int
+        '''
+        self._client = None
+        self._queue = []
+        self._refcount_dict = {}
+        self._current_player = None
+        self._player_volume = 50.0
+        # sleep delay for bot reponse time
+        self._roundtrip_delay = delay
+
+    def get_queue(self):
+        return self._queue
+
+    async def play(self, bot, bot_voice, message):
+        '''(SenpaiPlayer, Client, VoiceClient, Message) -> None
+        Plays songs in the local queue.
+        '''
+        await asyncio.sleep(self._roundtrip_delay)
+        # play songs while there are songs in the queue
+        while (self._queue):
+            # pop the next song off the queue
+            song = self.get_song(0)
+
+            # retrieve song
+            player = bot_voice.create_ffmpeg_player(song.path)
+
+            # print a message showing what is currently playing
+            bot_reply = ("`Playing: \"" + song.title + "\"`")
+            await bot.say(bot_reply)
+
+            self._current_player = player
+            # set player volume
+            self.set_volume(self._player_volume)
+            # play the song
+            player.start()
+            # if the bot is no longer playing anything, stop the player and leave
+            while (not player.is_done()):
+                await asyncio.sleep(self._roundtrip_delay)
+            player.stop()
+            self.pop_song(0)
+
+        # output message saying bot has left and leave
+        bot_reply = "`Leaving voice channel`"
+        await bot.say(reply)
+        await bot_voice.disconnect()
+
+    async def skip(self):
+        '''(SenpaiPlayer) -> None
+        Skip the current song in all queues
+        '''
+        if (self._current_player):
+            self._current_player.stop()
+
+    async def pause(self):
+        '''(SenpaiPlayer) -> None
+        Pauses all songs in all queues
+        '''
+        if (self._current_player):
+            self._current_player.pause()
+
+    async def resume(self):
+        '''(SenpaiPlayer) -> null
+        Resumes all songs in all queues
+        '''
+        if (self._current_player):
+            self._current_player.resume()
+
+    def get_volume(self):
+        '''(SenpaiPlayer) -> float
+        Returns the volume for this player
+        '''
+        return self._player_volume
+
+    def set_volume(self, volume):
+        '''(SenpaiPlayer, float) -> None
+        Sets the volume for this player in all queues
+        '''
+        self._player_volume = volume
+        if (self._current_player):
+            self._current_player.volume = self._player_volume / 50
+
+    def clear_queue(self):
+        '''(SenpaiPlayer) -> float
+        Clears the queue and local queue of this player
+        '''
+        self._queue.clear()
+        self._refcount_dict.clear()
+
+    def add_song(self, url):
+        song = None
+        if (url in self._refcount_dict):
+            song = self._refcount_dict[url][0]
+            ref = self._refcount_dict[url][1]
+            self._refcount_dict[url] = (song, ref+1)
+        else:
+            song = make_local_song_from_url(url)
+            if (not os.path.isfile(song.path)):
+                download_youtube.download_song(url)
+            self._refcount_dict[url] = (song, 1)
+        self._queue.append(song)
+        return song
+
+    def get_song(self, queue_num):
+        return self._queue[queue_num]
+
+    def pop_song(self, queue_num):
+        song = self._queue.pop(queue_num)
+        ref = self._refcount_dict[song.url][1] - 1
+        if (ref <= 0):
+            self._refcount_dict.pop(song.url, None)
+            if (os.path.isfile(song.path)):
+                print("removing file: " + song.path)
+                os.remove(song.path)
+        else:
+            self._refcount_dict[song.url] = (song, ref)
+        return song
+
 
 class SenpaiPlayer:
 
@@ -11,51 +143,20 @@ class SenpaiPlayer:
         self._client = None
         # song queue, list of string
         self._queue = []
-        # local song queue, list of SenpaiSong
-        self._local_queue = []
-        # list of players
-        self._player_list = []
+
+        # reference counter dictionary
+        self._refcount_dict = {}
+
+        self._current_player = None
+
         self._player_volume = 50.0
         # sleep delay for bot reponse time
         self._roundtrip_delay = delay
 
-    async def leave_all_voice_channels(self, bot):
-        '''(SenpaiPlayer, Client) -> None
-        makes the Client leave all connected voice channels
-        '''
+    def get_queue(self):
+        return self._queue
 
-        connected_voices = []
-        # get a list of all voice channels the bot is connected to
-        for voice in bot.voice_clients:
-            # if bot is connected to the channel, add it to the list
-            if (voice.is_connected()):
-                connected_voices.append(voice)
-        # if bot is not connected to any voice channel, print error message
-        if (connected_voices):
-            # disconnect bot from all connected voice channels
-            for voice in connected_voices:
-                await voice.disconnect()
-
-    async def join_voice_channel_of_user(self, message, bot):
-        '''(SenpaiPlayer, Message, Client) -> VoiceClient
-        given the message of a user, join their voice channel if they are in one
-        '''
-
-        bot_voice = None
-        # get the sender of the message
-        author = message.author
-        # get their current voice channel
-        cur_author_vchannel = author.voice.voice_channel
-        # if they are in a voice channel, join it
-        if (cur_author_vchannel):
-            bot_voice = await bot.join_voice_channel(cur_author_vchannel)
-        else:
-            raise UserNotInVoiceChannelException(author +
-                " is not in a voice channel")
-
-        return bot_voice
-
-    async def play_song(self, bot, bot_voice, message):
+    async def play(self, bot, bot_voice, message):
         '''(SenpaiPlayer, Client, VoiceClient, Message) -> None
         Plays songs in the queue.
         '''
@@ -63,115 +164,99 @@ class SenpaiPlayer:
         # play songs while there are songs in the queue
         while (self._queue):
             # pop the next song off the queue
-            song = self._queue.pop(0)
+            song = self.get_song(0)
             # retrieve song
             player = await bot_voice.create_ytdl_player(song.path)
-            song.title = player.title
+
+            # print a message showing what is currently playing
+            bot_reply = ("`Playing: \"" + song.title + "\"`")
+            await bot.say(bot_reply)
+
+            self._current_player = player
             # play the song
             player.start()
             # set player volume
-            self.player_set_volume(self._player_volume)
-            self._player_list.append(player)
+            self.set_volume(self._player_volume)
 
-            # print a message showing what is currently playing
-            reply = ("`Playing: \"" + song.title + "\"`")
-            await bot.send_message(message.channel, reply)
             # if the bot is no longer playing anything, stop the player and leave
             while (not player.is_done()):
                 await asyncio.sleep(self._roundtrip_delay)
             player.stop()
-            self._player_list.pop()
+            self.pop_song(0)
+
         # output message saying bot has left and leave
-        reply = "`Leaving voice channel.`"
-        await bot.send_message(message.channel, reply)
-        await self.leave_all_voice_channels(bot)
+        bot_reply = "`Leaving voice channel`"
+        await bot.say(reply)
+        await bot_voice.disconnect()
 
-    async def play_song_local(self, bot, bot_voice, message):
-        '''(SenpaiPlayer, Client, VoiceClient, Message) -> None
-        Plays songs in the local queue.
-        '''
-        await asyncio.sleep(self._roundtrip_delay)
-        # play songs while there are songs in the queue
-        while (self._local_queue):
-            # pop the next song off the queue
-            song = self._local_queue[0]
-
-            # retrieve song
-            player = bot_voice.create_ffmpeg_player(song.path)
-            # set player volume
-            self.player_set_volume(self._player_volume)
-            # play the song
-            player.start()
-            # add player to list of playing players
-            self._player_list.append(player)
-            # print a message showing what is currently playing
-            reply = ("`Playing: \"" + song.title + "\"`")
-            await bot.send_message(message.channel, reply)
-            # if the bot is no longer playing anything, stop the player and leave
-            while (not player.is_done()):
-                await asyncio.sleep(self._roundtrip_delay)
-            player.stop()
-            song.delete_local()
-            self._player_list.pop()
-            self._local_queue.pop(0)
-        # output message saying bot has left and leave
-        reply = "`Leaving voice channel.`"
-        await bot.send_message(message.channel, reply)
-        await self.leave_all_voice_channels(bot)
-
-    async def player_skip(self):
+    async def skip(self):
         '''(SenpaiPlayer) -> None
         Skip the current song in all queues
         '''
-        for player in self._player_list:
-            player.stop()
+        if (self._current_player):
+            self._current_player.stop()
 
-    async def player_pause(self):
+    async def pause(self):
         '''(SenpaiPlayer) -> None
         Pauses all songs in all queues
         '''
-        for player in self._player_list:
-            player.pause()
+        if (self._current_player):
+            self._current_player.pause()
 
-    async def player_resume(self):
+    async def resume(self):
         '''(SenpaiPlayer) -> null
         Resumes all songs in all queues
         '''
-        for player in self._player_list:
-            player.resume()
+        if (self._current_player):
+            self._current_player.resume()
 
-    def player_get_volume(self):
+    def get_volume(self):
         '''(SenpaiPlayer) -> float
         Returns the volume for this player
         '''
         return self._player_volume
 
-    def player_clear(self):
+    def set_volume(self, volume):
+        '''(SenpaiPlayer, float) -> None
+        Sets the volume for this player in all queues
+        '''
+        self._player_volume = volume
+        if (self._current_player):
+            self._current_player.volume = self._player_volume / 50
+
+    def clear_queue(self):
         '''(SenpaiPlayer) -> float
         Clears the queue and local queue of this player
         '''
         self._queue.clear()
-        self._local_queue.clear()
+        self._refcount_dict.clear()
 
-    def player_set_volume(self, volume):
-        '''(SenpaiPlayer, float) -> None
-        Sets the volume for this player in all queues
-        '''
-        for player in self._player_list:
-            player.volume = volume / 50
-
-    def add_song(self, song):
+    def add_song(self, url):
         '''(SenpaiPlayer, String) -> None
         Add a song to the queue
         '''
+        song = None
+        if (url in self._refcount_dict):
+            song = self._refcount_dict[url][0]
+            ref = self._refcount_dict[url][1]
+            self._refcount_dict[url] = (song, ref+1)
+        else:
+            song = make_song_from_url(url)
+            self._refcount_dict[url] = (song, 1)
         self._queue.append(song)
 
-    def add_song_local(self, song):
-        self._local_queue.append(song)
+        return song
 
-    def get_queue(self):
-        return self._queue
+    def get_song(self, queue_num):
+        return self._queue[queue_num]
 
-    def get_localqueue(self):
-        return self._local_queue
+    def pop_song(self, queue_num):
+        song = self._queue.pop(queue_num)
+        ref = self._refcount_dict[song.url][1] - 1
+        if (ref <= 0):
+            self._refcount_dict.pop(song.url, None)
+        else:
+            self._refcount_dict[song.url] = (song, ref)
+
+        return song
 
--- src/senpai_song.py
+++ src/senpai_song.py
@@ -1,20 +1,13 @@
-import os
-
 class SenpaiSong:
 
-    def __init__(self, path, title="No Song Title"):
+    def __init__(self):
         '''(SenpaiSong, str, str) -> SenpaiSong
         path: where to find the song. can be https:// or normal path.
         title: title of song.
         '''
-        self.path = path
-        self.title = title
-        self.ref = 1
-
-    def delete_local(self):
-        self.ref -= 1
-        if (self.ref == 0):
-            os.remove(self.path)
+        self.url = None
+        self.path = None
+        self.title = None
 
     def __str__(self):
         return self.title
