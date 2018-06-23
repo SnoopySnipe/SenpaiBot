00d7dd05e8716f2a8ecb3653cf4fb1f706d84a4d
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-06-22 21:49:57 -0400

    port senpai_imageboards.py to rewrite - add support for embed rich text

--- src/senpai_imageboards.py
+++ src/senpai_imageboards.py
@@ -1,53 +1,71 @@
 import random
 import requests
 
+import discord
+
 from discord.ext import commands
 
 
 class SenpaiImageboard:
 
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.group(pass_context=True)
+    @commands.group()
     async def daily(self, context):
         if (context.invoked_subcommand is None):
             imageboards = [self.yandere, self.danbooru,
                            self.konachan, self.gelbooru]
-            await random.choice(imageboards).invoke(context)
+            await random.choice(imageboards).reinvoke(context)
 
     @daily.command()
-    async def help(self):
+    async def help(self, context):
         reply = ("`usage:`\n" +
                 "`!senpai daily\n" + "!senpai daily yandere\n" +
                 "!senpai daily danbooru\n" + "!senpai daily konachan\n" +
                 "!senpai daily gelbooru\n`")
-        await self.bot.say(reply)
+        await context.send(reply)
 
     @daily.command()
-    async def yandere(self):
-        json_content = _yandere_get_latest_post()
-        if (json_content is None):
-            await self.bot.say("Error: API down?")
+    async def yandere(self, context):
+        # link for yande.re's json api
+        api_url = "https://yande.re/post.json?limit=1"
+        post_url = "https://yande.re/post/show/{}"
+
+        # get json contents
+        json_content = requests.get(api_url).json()
+        if (len(json_content) <= 0):
+            await context.send("Error: API down?")
             return
+
+        json_content = json_content[0]
+
         if ("id" not in json_content or "sample_url" not in json_content):
-            await self.bot.say("Error: json parse failed")
+            await context.send("Error: failed to parse json")
             return
 
         post_id = json_content["id"]
         file_url = json_content["sample_url"]
 
-        bot_reply = "`yandere #" + str(post_id) + "`\n" + file_url
-        await self.bot.say(bot_reply)
+        embed_msg = discord.Embed(title="yandere: #{}".format(post_id),
+                            url=post_url.format(post_id),
+                            color=0xff93ac)
+        embed_msg.set_image(url=file_url)
+
+        await context.send(embed=embed_msg)
 
     @daily.command()
-    async def danbooru(self):
-        json_content = _danbooru_get_latest_post()
-        if (json_content is None):
-            await self.bot.say("Error: API down?")
+    async def danbooru(self, context):
+        api_url = "http://danbooru.donmai.us/posts.json?limit=1"
+        post_url = "https://danbooru.donmai.us/posts/{}"
+
+        # get json contents
+        json_content = requests.get(api_url).json()
+        if (len(json_content) <= 0):
+            await context.send("Error: API down?")
             return
+
+        json_content = json_content[0]
+
         if ("id" not in json_content or "file_url" not in json_content):
-            await self.bot.say("Error: json parse failed")
+            await context.send("Error: failed to parse json")
             return
 
         post_id = json_content["id"]
@@ -55,112 +73,67 @@ class SenpaiImageboard:
         if ("donmai.us" not in file_url):
             file_url = "https://danbooru.donmai.us" + file_url
 
-        bot_reply = "`danbooru #" + str(post_id) + "`\n" + file_url
-        await self.bot.say(bot_reply)
+        embed_msg = discord.Embed(title="danbooru: #{}".format(post_id),
+                            url=post_url.format(post_id),
+                            color=0xff93ac)
+        embed_msg.set_image(url=file_url)
+
+        await context.send(embed=embed_msg)
 
     @daily.command()
-    async def gelbooru(self):
-        json_content = _gelbooru_get_latest_post()
-        if (json_content is None):
-            await self.bot.say("Error: API down?")
+    async def gelbooru(self, context):
+        api_url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1"
+        post_url = "https://gelbooru.com/index.php?page=post&s=view&id={}"
+
+        # get json contents
+        json_content = requests.get(api_url).json()
+        if (len(json_content) <= 0):
+            await context.send("Error: API down?")
             return
+
+        json_content = json_content[0]
+
         if ("id" not in json_content or "file_url" not in json_content):
-            await self.bot.say("Error: json parse failed")
+            await context.send("Error: failed to parse json")
             return
 
         post_id = json_content["id"]
         file_url = json_content["file_url"]
 
-        bot_reply = "`gelbooru #" + str(post_id) + "`\n" + file_url
-        await self.bot.say(bot_reply)
+        embed_msg = discord.Embed(title="gelbooru: #{}".format(post_id),
+                            url=post_url.format(post_id),
+                            color=0xff93ac)
+        embed_msg.set_image(url=file_url)
+
+        await context.send(embed=embed_msg)
 
     @daily.command()
-    async def konachan(self):
-        json_content = _konachan_get_latest_post()
-        if (json_content is None):
-            await self.bot.say("Error: API down?")
+    async def konachan(self, context):
+        # link for konachan.com's json api
+        api_url = "https://konachan.com/post.json?limit=1"
+        post_url = "http://konachan.com/post/show/{}"
+
+        # get json contents
+        json_content = requests.get(api_url).json()
+        if (len(json_content) <= 0):
+            await context.send("Error: API down?")
             return
+
+        json_content = json_content[0]
+
         if ("id" not in json_content or "file_url" not in json_content):
-            await self.bot.say("Error: json parse failed")
+            await context.send("Error: failed to parse json")
             return
 
         post_id = json_content["id"]
         file_url = json_content["sample_url"]
 
-        bot_reply = "`konachan #" + str(post_id) + "`\n" + file_url
-        await self.bot.say(bot_reply)
+        embed_msg = discord.Embed(title="konachan: #{}".format(post_id),
+                            url=post_url.format(post_id),
+                            color=0xff93ac)
+        embed_msg.set_image(url=file_url)
 
+        await context.send(embed=embed_msg)
 
 def setup(bot):
-    bot.add_cog(SenpaiImageboard(bot))
-
-
-def _yandere_get_latest_post():
-    '''(void) -> dict
-
-
-    '''
-
-    # link for yande.re's json api
-    api_url = "https://yande.re/post.json?limit=1"
-
-    # get json contents
-    json_content = requests.get(api_url).json()
-    if (len(json_content) > 0):
-        return json_content[0]
-
-
-def _danbooru_get_latest_post():
-
-    api_url = "http://danbooru.donmai.us/posts.json?limit=1"
-
-    # get json contents
-    json_content = requests.get(api_url).json()
-    if (len(json_content) > 0):
-        return json_content[0]
-
-
-def _gelbooru_get_latest_post():
-
-    api_url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1"
-
-    json_content = requests.get(api_url).json()
-    if (len(json_content) > 0):
-        return json_content[0]
-
-
-def _konachan_get_latest_post():
-    '''(void) -> dict
-
-
-    '''
-
-    # link for konachan.com's json api
-    api_url = "https://konachan.com/post.json?limit=1"
-
-    # get json contents
-    json_content = requests.get(api_url).json()
-    if (len(json_content) > 0):
-        return json_content[0]
-
-
-if (__name__ == "__main__"):
-    print("Yandere:")
-    json_content = yandere_get_latest_post()
-    print(json_content["id"])
-    print(json_content["sample_url"])
-
-    print("Danbooru:")
-    json_content = danbooru_get_latest_post()
-    print(json_content["id"])
-    print(json_content["file_url"])
-
-    print("Gelbooru")
-    json_content = gelbooru_get_latest_post()
-    print(json_content["id"])
-    print(json_content["file_url"])
-
-    print("Konachan:")
-    json_content = konachan_get_latest_post()
-    print(json_content["id"])
-    print(json_content["sample_url"])
+    bot.add_cog(SenpaiImageboard())
