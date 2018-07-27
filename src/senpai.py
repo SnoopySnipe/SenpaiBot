c512ef798e7a298c3316f1205515ab8d54fbbbe1
Alex Wong <alex.wong.utsc@gmail.com>
2018-07-27 14:02:32 -0400

    Added logging for status checking and debugging
    discord.log can now be opened to check senpaibot logs

--- src/senpai.py
+++ src/senpai.py
@@ -4,6 +4,14 @@ import asyncio
 
 from discord.ext import commands
 
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 DESCRIPTION = '''The senpai of the server.'''
 
 # initialize bot
