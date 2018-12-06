f63fd557733cd532c825fce6ee5daf562a0f50eb
Alex Wong <alex.wong.utsc@gmail.com>
2018-12-06 13:52:55 -0500

    Update senpai_events.py

--- src/senpai_events.py
+++ src/senpai_events.py
@@ -3,6 +3,44 @@ import discord
 from discord.ext import commands
 from helpers import *
 class SenpaiEvents:
+    class Event_List:
+        def __init__(self):
+            self.event_list = []
+        def add_event(self, event_name, event_start_time):
+            self.event_list.append(Event(event_name, event_start_time))
+        def list_events(self):
+            res = ""
+            for i in range(len(self.event_list)):
+                res += "#" + str(i) + ". " + self.event_list[i].event_name
+            return res
+        def view_attendees(self, eventIndex):
+            if(eventIndex < len(self.event_list)):
+            return self.event_list[eventIndex].view_attendees()
+        def add_attendee(self, eventIndex, name):
+            if(eventIndex < len(self.event_list)):
+                return self.event_list[eventIndex].add_attendee()
+        def remove_attendee(self, eventIndex, name):
+            if(eventIndex < len(self.event_list)):
+                return self.event_list[eventIndex].remove_attendee()
+    class Event:
+        def __init__(self, event_name, event_start_time):
+            self.event_name = event_name
+            self.event_start_time = event_start_time
+            self.attendees = []
+        def view_attendees(self):
+            return self.attendees
+        def add_attendee(self, name):
+            if(name not in self.attendees):
+                self.attendees.append(name)
+                return "Added to " + self.event_name
+            else:
+                return "Already added to event"
+        def remove_attendee(self, name):
+            if(name in self.attendees):
+                self.attendees.remove(name)
+                return "Removed from " + self.event_name
+            else:
+                return "Already removed from event"
     event_list = Event_List()
     def __init__(self, bot):
         self.bot = bot
