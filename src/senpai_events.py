9d5ffd3d1bc5b742172e6d83c755191c030321a1
Alex Wong <alex.wong.utsc@gmail.com>
2018-12-06 14:24:39 -0500

    Update senpai_events.py

--- src/senpai_events.py
+++ src/senpai_events.py
@@ -7,7 +7,7 @@ class SenpaiEvents:
 
     # Manages events
     @commands.command(name="event")
-    async def _event(self, context):
+    async def _event(self, context, *args):
         offset = len("!senpai event")
 
         question = context.message.content[offset+1:]
@@ -15,7 +15,6 @@ class SenpaiEvents:
         if (len(question) == 0):
             await context.send("`Use !senpai event list, !senpai event create [event name] [event time], !senpai event join [event number], or !senpai event leave [event number]`")
             return
-        args = question.split();
         res = ""
         if(args[0] == "create"):
             res = event_list.add_event(args[1], args[2])
