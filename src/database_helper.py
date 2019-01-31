86a0d87822068efc9869145162a2a3e795b3f133
RarePep8 <alex.wong.utsc@gmail.com>
2019-01-31 18:35:46 -0500

    Fixed database helpers missing generic db name

--- src/database_helper.py
+++ src/database_helper.py
@@ -1,17 +1,19 @@
 import database
 import sqlite3
+db = 'foo.db'
 def initialize(server_name):
-    conn = sqlite3.connect(server_name + '.db')
+    db = server_name + '.db'
+    conn = sqlite3.connect(db)
     database.initialize(conn)
     conn.close()
 def add_pikapoints(user_id, points):
-    conn = sqlite3.connect('pikapoints.db')
+    conn = sqlite3.connect(db)
     if(conn is not None):
         database.add_pikapoints_query(conn, user_id, points)
         conn.close()
 
 def get_pikapoints(user_id):
-    conn = sqlite3.connect('pikapoints.db')
+    conn = sqlite3.connect(db)
     result = None
     if(conn is not None):
         result = database.get_pikapoints_query(conn, user_id)
@@ -21,7 +23,7 @@ def get_pikapoints(user_id):
         return result
 
 def get_pikalogue():
-    conn = sqlite3.connect('pikapoints.db')
+    conn = sqlite3.connect(db)
     if(conn is not None):
         result = database.get_pikalogue(conn)
         conn.close()
