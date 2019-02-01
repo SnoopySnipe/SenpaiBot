9ae705f8270cbecec6d164fc5b8226f5c3af22dd
Andrew Leung <aleung@ci.com>
2019-02-01 10:41:38 -0500

    roll

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -34,7 +34,13 @@ class SenpaiGacha:
         if details is None:
             await context.send("You have no pikapoints! Join voice and start earning!")
         elif PRICE <= details[0]:
-            r = random.randint(1, 1000)
+            r = random.randint(0, 1003)
+            if r == 0:
+                options = database_helper.get_roll(7)
+                database_helper.adjust_pity(context.message.author.id, True)
+            elif 1001 <= r <= 1003:
+                options = database_helper.get_roll(6)
+                database_helper.adjust_pity(context.message.author.id, True)
             if 1 <= r <= details[1]:
                 options = database_helper.get_roll(3)
                 database_helper.adjust_pity(context.message.author.id, False)
