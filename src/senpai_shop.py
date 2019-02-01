1b98eb30ab9dac1aa05638dc750f7c4139e29192
SnoopySnipe <andrewleung.41898@outlook.com>
2019-01-31 20:08:15 -0500

    fix

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -17,11 +17,17 @@ class SenpaiShop:
     @commands.command(name="pity")
     async def pity(self, context):
         pity = database_helper.get_pity(context.message.author.id)
-        
         title = "{}'s Pity Rates: \n".format(context.message.author.name)
-        description = "3*: {}\n4*: {}\n5*: {}\nFocus: {}".format(pity[0], pity[1], pity[2], pity[3])
+        if pity is None:
+            description = "3*: 40%\n4*: 50%\n5*: 4%\nFocus: 6%"
+        else:
+            description = "3*: {}\n4*: {}\n5*: {}\nFocus: {}".format(pity[0], pity[1], pity[2], pity[3])
             
         await context.send(embed=discord.Embed(title=title, description=description, color=0x9370db))
+
+    #@commands.command(name="roll"
+    #async def roll(self, context):
+        
 
 
 def setup(bot):
