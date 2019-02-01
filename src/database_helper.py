279357f8bf03c7f19799f665de250d6af565cb97
Andrew Leung <aleung@ci.com>
2019-02-01 14:22:28 -0500

    big bug fix

--- src/database_helper.py
+++ src/database_helper.py
@@ -86,6 +86,7 @@ def get_from_inventory(user_id, poke_id):
     conn = sqlite3.connect(db)
     result = False
     if(conn is not None):
+        print(database.get_from_inventory(conn, user_id, poke_id))
         if database.get_from_inventory(conn, user_id, poke_id) is not None:
             result = True
         conn.close()
