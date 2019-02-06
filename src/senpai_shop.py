2e91927258865c5342a6faa295b7e821a076b871
Leung <aleung@hjones.corporate.ciglobe.net>
2019-02-06 16:58:07 -0500

    pokedex

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -206,10 +206,10 @@ class SenpaiGacha:
     @commands.command(name="pokedex")
     async def pokedex(self, context, name):
         poke_id = database_helper.get_pokemon(name)
-        if poke_id[0] is not None:
+        if poke_id is not None:
             str_id = "{:03}".format(poke_id[0])
             url = "https://www.serebii.net/sunmoon/pokemon/{}.png".format(str_id)
-            dex = discord.Embed(title="ID: {}\nName: {}".format(str_id, name), color=0xffb6c1)
+            dex = discord.Embed(title="ID: {}\nName: {}".format(str(poke_id), name), color=0xffb6c1)
             dex.set_image(url=url)
             await context.send(embed=dex)
         else:
