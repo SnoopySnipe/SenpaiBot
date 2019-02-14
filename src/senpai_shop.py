1e95b1af4eaddc3754caeafdcb0fd7c1bd6a4d99
RarePep8 <alex.wong.utsc@gmail.com>
2019-02-14 21:14:58 +0000

    add hoenn to quiz

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -668,7 +668,7 @@ class SenpaiGacha:
                     await channel.send("{} is on a {}-streak! Next quiz will be at approximately {}:{:02}. Shut them down!".format(self.bot.get_user(high_streak[0]).name, high_streak[1], display_hour, next_quiz.minute))
             await asyncio.sleep(t) # generate quizzes every 10 - 30 minutes
             if not 4 < datetime.datetime.now().hour < 13: # generate quizzes only from 8am - 12am
-                r = random.randint(1, 251) # generate random pokemon
+                r = random.randint(1, 386) # generate random pokemon
                 pokemon = database_helper.get_pokemon_name(r)[0]
                 str_id = "{:03}".format(r)
                 url = "https://www.serebii.net/sunmoon/pokemon/{}.png".format(str_id)
