729c970563847e14b2f91ee99949923d14c382c0
Leung <aleung@WebservessiMac2.corporate.ciglobe.net>
2019-03-04 14:36:48 -0500

    bag

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -445,14 +445,16 @@ class SenpaiGacha:
             item_id = bag[0]
             # sprite = pb.SpriteResource('pokemon', pokemon_id)
             # img = Image.open(sprite.path).convert("RGBA")
-            if item_id == '1':
+            if item_id == 1:
                 item_str = 'pokeball'
-            elif item_id == '2':
+            elif item_id == 2:
                 item_str = 'greatball'
-            elif item_id == '3':
+            elif item_id == 3:
                 item_str = 'ultraball'
-            elif item_id == '4':
+            elif item_id == 4:
                 item_str = 'masterball'
+            print(item_id)
+            print(item_str)
             response = requests.get("https://www.serebii.net/itemdex/sprites/pgl/{}.png".format(item_str))
             img = Image.open(BytesIO(response.content)).convert("RGBA")
             img = img.resize((150, 150))
