b7e83dbd558092d151bfc2524c364f4544f7b3b3
aleung <aleung@ci.com>
2019-03-22 15:24:48 -0400

    added team_split

--- src/senpai_shop.py
+++ src/senpai_shop.py
@@ -2274,7 +2274,7 @@ class SenpaiGacha:
         if(channel is None):
             return
         while True:
-            t = random.randint(600, 1800)
+            t = random.randint(30, 30)
             high_streak = database_helper.get_high_streak()
             if high_streak is not None:
                 next_quiz = datetime.datetime.now() + datetime.timedelta(seconds=t)
